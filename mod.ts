3288547ee8baa656bf647001bd5293a373fc88f8
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-03-03 12:55:21 +0000

    add docs

--- mod.ts
+++ mod.ts
@@ -2,6 +2,13 @@
 declare var Bun: any;
 declare var process: any;
 
+/**
+ * Get the environment variable value
+ * @example
+ * ```ts
+ * import * as env from "jsr:@tsirysndr/env-js";
+ * console.log(env.get("HOME")); // /home/user
+ */
 export function get(key: string): string | undefined {
   if (Deno) {
     return Deno.env.get(key);
@@ -18,6 +25,15 @@ export function get(key: string): string | undefined {
   return undefined;
 }
 
+/**
+ * Verify if the environment variable exists
+ *
+ * @example
+ * ```ts
+ * import * as env from "jsr:@tsirysndr/env-js";
+ * console.log(env.has("HOME")); // true
+ * ```
+ */
 export function has(key: string): boolean {
   if (Deno) {
     return Deno.env.has(key);
